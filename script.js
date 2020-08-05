815429fd1cab8f1363c76be3f5d8ec9c214a2b83
Sharifur Rahman <robinmedia7@gmail.com>
2020-08-05 15:46:18 +0600

    JS TYPING EFFECT

---
+++ script.js
@@ -0,0 +1,29 @@
+const texts = [
+    'websites','e-commerce','omletes'
+]
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+(function type()
+{
+   if(count===texts.length) 
+   {
+    count = 0
+   } 
+
+   currentText = texts[count];
+   letter = currentText.slice(0,++index);
+
+   document.querySelector('.typing').textContent = letter;
+
+   if(letter.length===currentText.length)
+   {
+       count++;
+       index = 0;
+   }
+   
+   setTimeout(type,300)
+
+})()
